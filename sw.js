8144f3b94ab818212424d778696cbfa5e0cf0342
akashvarma2 <akash.risma@gmail.com>
2026-04-29 19:58:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,39 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
+const CACHE_NAME = 'pulp-pro-v1';
+const ASSETS = [
+  '/Pulp/',
+  '/Pulp/index.html',
+  '/Pulp/edited-image.png',
+  '/Pulp/manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css'
+];
+
+// Install Event: Caches the essential files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Pulp Pro: Caching Shell Assets');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+// Activate Event: Cleans up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serves assets from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
